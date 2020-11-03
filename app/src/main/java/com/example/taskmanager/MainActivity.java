fd525165f3ebc9c3767bede1ecf1aac5cdef0339
Garhett Morgan <32fossil@gmail.com>
2020-11-02 20:59:35 -0800

    still unfinished need to work out amplify

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Build;
@@ -9,7 +11,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Task> tasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,30 +36,37 @@ public class MainActivity extends AppCompatActivity {
 //                notificationManager.createNotificationChannel(channel);
 //            }
 //        }
+        setTheTask();
+        System.out.println(tasks.toString());
+        RecyclerView rView = findViewById(R.id.recyclerView);
+        rView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rView.setAdapter(new TaskViewAdapter(tasks));
 
-        Button taskOne = MainActivity.this.findViewById(R.id.taskOne);
-        taskOne.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
-            String taskName = taskOne.getText().toString();
-            i.putExtra("task", taskName);
-            startActivity(i);
-        });
 
-        Button taskTwo = MainActivity.this.findViewById(R.id.taskTwo);
-        taskTwo.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
-            String taskName = taskTwo.getText().toString();
-            i.putExtra("task", taskName);
-            startActivity(i);
-        });
 
-        Button taskThree = MainActivity.this.findViewById(R.id.taskThree);
-        taskThree.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
-            String taskName = taskThree.getText().toString();
-            i.putExtra("task", taskName);
-            startActivity(i);
-        });
+//        Button taskOne = MainActivity.this.findViewById(R.id.taskOne);
+//        taskOne.setOnClickListener(v -> {
+//            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
+//            String taskName = taskOne.getText().toString();
+//            i.putExtra("task", taskName);
+//            startActivity(i);
+//        });
+//
+//        Button taskTwo = MainActivity.this.findViewById(R.id.taskTwo);
+//        taskTwo.setOnClickListener(v -> {
+//            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
+//            String taskName = taskTwo.getText().toString();
+//            i.putExtra("task", taskName);
+//            startActivity(i);
+//        });
+//
+//        Button taskThree = MainActivity.this.findViewById(R.id.taskThree);
+//        taskThree.setOnClickListener(v -> {
+//            Intent i = new Intent(MainActivity.this, TaskDetailPage.class);
+//            String taskName = taskThree.getText().toString();
+//            i.putExtra("task", taskName);
+//            startActivity(i);
+//        });
         
         Button addTaskButton = MainActivity.this.findViewById(R.id.addTaskButton);
         addTaskButton.setOnClickListener(v -> {
@@ -67,5 +79,13 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, AllTasks.class);
             startActivity(i);
         });
+    }
+
+    public void setTheTask() {
+        tasks.add(new Task("Do Dishes", "Do the dishes and clean counters", "inprogress"));
+        tasks.add(new Task("Do laundry", "Clean al lclothes", "inprogress"));
+        tasks.add(new Task("Do homework", "study hard for the money", "inprogress"));
+        tasks.add(new Task("Do cat litter", "change cat litter", "inprogress"));
+        tasks.add(new Task("Do chores", "Do everything else", "inprogress"));
     }
 }
---
+++ app/src/main/java/com/example/taskmanager/Task.java
@@ -0,0 +1,14 @@
+package com.example.taskmanager;
+
+public class Task {
+
+    String taskName;
+    String taskDesc;
+    String taskProgress;
+
+    public Task(String taskName, String taskDesc, String taskProgress) {
+        this.taskName = taskName;
+        this.taskDesc = taskDesc;
+        this.taskProgress = taskProgress;
+    }
+}
